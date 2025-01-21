5c3b06c64d76e2401c733b49fbfae3681af8ba7b
Pablo <pablog@workair.ie>
2025-01-21 21:12:39 +0100

    add role tag

--- shared/auth/domain/resource/resource_permission.go
+++ shared/auth/domain/resource/resource_permission.go
@@ -1,8 +1,8 @@
 package resource
 
 type ResourcePermission struct {
-	ID          string `json:"id,omitempty"`
-	Name        string `json:"name"`
-	DisplayName string `json:"displayName,omitempty"`
-	Roles       map[string][]string
+	ID          string              `json:"id,omitempty"`
+	Name        string              `json:"name"`
+	DisplayName string              `json:"displayName,omitempty"`
+	Roles       map[string][]string `json:"roles,omitempty"`
 }
