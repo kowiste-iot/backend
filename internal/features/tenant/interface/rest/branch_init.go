7a2d98a8173bc2b1ec0c87ab16abe90ebf443003
Pablo <pablog@workair.ie>
2025-02-22 16:23:44 +0100

    routes fix

--- internal/features/tenant/interface/rest/branch_init.go
+++ internal/features/tenant/interface/rest/branch_init.go
@@ -23,12 +23,12 @@ func NewBranch(base *base.BaseService, branchService app.BranchService, middlewa
 }
 
 func (bh *BranchHandler) Init(rg *gin.RouterGroup) *gin.RouterGroup {
-	apiBranch := rg.Group("branchid")
+	branchID := rg.Group(":branchid")
 	{
-		apiBranch.Use(bh.middleware.Recovery())
-		apiBranch.Use(bh.middleware.Auth())
-		// Branch routes here
+		branchID.Use(bh.middleware.Recovery())
+		branchID.Use(bh.middleware.Auth())
+	
 	}
-	branchID := apiBranch.Group(":branchid")
+
 	return branchID
 }
